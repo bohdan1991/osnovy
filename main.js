3619a30c3bbd829178a2266add8c97a16d8b9784
bohdan1991 <bodya_med@ukr.net>
2019-11-07 17:47:27 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,35 @@
+// const name = prompt("Your name", 0);
+// alert(`Hello ${name} `);
+
+// const age = +prompt("How old are you?")
+// const salaryTotal = 2019 - age;
+// alert (`You born in ${salaryTotal}.`);
+
+// const long = prompt("Howe long?");
+// const salaryTotal = long * 4; 
+// alert (`You square hes perimetre ${salaryTotal}.`);
+
+// const long = prompt("Howe long circle?");
+// const salaryTotal = long * long * 3.14; 
+// alert (`You circle hes area ${salaryTotal}.`);
+
+// const mile = +prompt("Num 1", 0);
+// const houers = +prompt("Num 2", 0);
+// const salaryTotal = mile / houers;
+// alert (`your speed ${salaryTotal} km/h`);
+
+// const many = prompt ("Howe many dolars in your hends")
+// const salaryTotal = many * (25 / 30);
+// alert (`you have ${salaryTotal} €`);
+
+const volume = prompt ("volume of your cart?")
+const salaryTotal = (volume * 1000) / 820;
+
+alert (`you have ${salaryTotal.toFixed(0)} €`)
+
+
+
+
+
+
+
